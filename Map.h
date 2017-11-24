d98de693539c0655eac0b396d08af0d8c1c3aa4c
Axel Sandstedt <32524004+OutermostFatCat@users.noreply.github.com>
2017-11-24 15:24:43 +0100

    Update Map.h

--- Map.h
+++ Map.h
@@ -1,6 +1,33 @@
 #ifndef HEADER_MAP_
 #define HEADER_MAP_
 
+#define START_SIZE 200
+#define SCREEN_X 51
+#define SCREEN_Y 15
+
+#define ATTACK 1
+#define PASSIVE 0
+
+
+#define MONSTER_HP 3
+
+/* monster data */
+typedef struct {
+	
+	int x_pos;
+	int y_pos;
+	int x_pending;
+	int y_pending;
+
+	unsigned int health;
+
+	int a_radius;
+
+	char m_texture;
+	char stand_texture;
+
+} monster;
+
 /* Character data */
 typedef struct {
 
@@ -14,6 +41,9 @@ typedef struct {
 	/* Abstract position */
 	int c_x;
 	int c_y;
+
+	/* if positive, attack target */
+	int attack;
 
 	/* char texture */
 	char char_texture;
@@ -47,10 +77,19 @@ void *thr_Mov(void *);
 void movement(character *);
 void *mov_exit(pthread_t *);
 void character_pos(char **map, character *man);
+void character_Attack(char **map, character *c, monster **m);
+int target_Monster (monster **m, int x, int y);
 
 /* Keyboard.c */
 void new_Inputmode(void);
 void old_Inputmode(void);
 void read_Scancode(char *);
+
+/* AI.c */
+monster *gen_Monster(char **map, character *c);
+monster *kill_Monster(char **map, monster *m);
+void mon_Position (char **map, monster *m);
+int check_Radius(monster *m, character *c);
+void mon_Follow(char **map, monster *m, character *c);
 
 #endif
